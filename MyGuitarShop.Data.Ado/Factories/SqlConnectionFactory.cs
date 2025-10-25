f91de82acc28eddbbe5c81059897ebc0b182b66f
maskless-777 <jonahdlee777@gmail.com>
2025-10-25 16:13:32 -0700

    SQLConnectionFactory created. Service added as singleton to the api.

---
+++ MyGuitarShop.Data.Ado/Factories/SqlConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace MyGuitarShop.Data.Ado.Factories
+{
+    public class SqlConnectionFactory(string connectionString)
+    {
+        public SqlConnection OpenSqlConnection()
+        {
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
